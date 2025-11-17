18a5841c9908e2fb76063e37835e7558353a238b
Nolan D'almeida <Nolan@example.com>
2025-11-17 11:20:17 +0100

    Imports php

---
+++ header.php
@@ -0,0 +1,13 @@
+ <div id="main">
+     <header>
+         <p>LOGO</p>
+         <nav>
+             <ol class="menu">
+                 <li><a href="accueil.php">Accueil</a></li>
+                 <li><a href="score.php">Scores</a></li>
+                 <li>
+                     <a href="contacte.php" class="bouton rouge">Nous Contacter</a>
+                 </li>
+             </ol>
+         </nav>
+     </header>
